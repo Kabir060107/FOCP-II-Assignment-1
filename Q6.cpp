d73b3ce30d0f546731dc608a1b4c2e5e993c7f07
Kabir060107 <24csu342@ncuindia.edu>
2025-02-11 10:00:11 +0530

    Q6.cpp

---
+++ Q6.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+ 
+    const int MULTIPLIER = 2;
+    
+    double length, width;
+  
+    cout << "Enter the length of the rectangle: ";
+    cin >> length;
+    
+    cout << "Enter the width of the rectangle: ";
+    cin >> width;
+    
+    double area = length * width;
+    double perimeter = MULTIPLIER * (length + width);
+    
+    cout << "Area of the rectangle: " << area << endl;
+    cout << "Perimeter of the rectangle: " << perimeter << endl;
+    
+    return 0;
+}
