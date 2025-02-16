88f3ac9db416699d6c67306c332b6f0e7c065471
Kabir060107 <24csu342@ncuindia.edu>
2025-02-16 13:34:33 +0530

    Q35.cpp

---
+++ Q35.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int gcd(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int lcm(int a, int b) {
+    return (a / gcd(a, b)) * b;
+}
+
+int main() {
+    int num1, num2;
+    cout << "Enter two integers: ";
+    cin >> num1 >> num2;
+    
+    cout << "GCD: " << gcd(num1, num2) << endl;
+    cout << "LCM: " << lcm(num1, num2) << endl;
+    
+    return 0;
+}
